8f09031b7d012668d770bfd4eec9c53c02becbe4
sriramrc-boop <sriramrameshch@gmail.com>
2026-04-16 16:52:47 +0530

    FileManager file created

---
+++ src/attendance/FileManager.java
@@ -0,0 +1,18 @@
+package attendance;
+
+public class FileManager {
+    private String path;
+
+    public void saveAttendance(Course c){
+
+    }
+
+    public Course retrieveData(String courseId){
+
+    }
+
+    public void getReport(){
+        
+    }
+
+}
