667a2f4f6912e6872bd5f5a1e8b266b00d400ba1
Anirudhsatish07 <anirudhsatish2007@gmail.com>
2026-04-26 10:06:13 +0530

    Update Course.java

--- src/attendance/Course.java
+++ src/attendance/Course.java
@@ -3,51 +3,92 @@ package attendance;
 import java.util.ArrayList;
 import java.util.Date;
 
-public class Course {
+public class Course{
+
     private String courseId;
     private String courseName;
     private int credit;
     private Instructor instructor;
-    private ArrayList<Student> studentList;
-    private ArrayList<Attendance> attendanceList;
+    private ArrayList<Attendence> attendenceList = new ArrayList<>();
+    private ArrayList<Student> studentList = new ArrayList<>();
 
-    public Course(){}
-    public Course(String courseId,String courseName,int credit){
-        this.courseId = courseId;
-        this.courseName = courseName;
-        this.credit = credit;
+    public course(){}
+
+    public course(String courseId, String courseName, int credit){
+
+    this.courseId = courseId;
+    this.courseName = courseName;
+    this.credit = credit;
+        
     }
 
-    public String getCourseId(){
-        return courseId;
+public String getCourseId(){
+
+return CourseId;
+        
     }
 
-    public String getCourseName(){
+public String getCourseName(){
+
         return courseName;
     }
+public void enrollStudent(Student s){
 
-    public void enrollStudent(Student s){
-        studentList.add(s);
-    }
-
-    public void setIntructor(Instructor i){
-        instructor = i;
-    }
-
-    public void markAttendance(Date d,Student s,boolean present){
-        Attendance a = new Attendance(s,this,d,present);
-        attendanceList.add(a);
-    }
-
-    public void getAttendancePercentage(){
-
-    }
-
-    public ArrayList<Attendance> getAttendanceList(){
-
-    }
-
-    public void getAttendanceReport(){
-
-    }
+    studentList.add(s);
+    
 }
+public void markAttendence(Date d,Student s,boolean present){
+
+Attendence a = new Attendence(s,this,d,present);
+    attendenceList.add(a);
+    
+}
+
+ public void setInstructor(Instructor i){
+
+this.instructor = i;
+     
+ }   
+
+  public void getAttendencePercentage(Student s){
+
+int totalClasses=0;
+int classepresent=0;
+
+for(Attendence r:attendenceList){
+
+if(r.getStudent().equals(s)){
+
+totalClasses++;
+
+    if (r.getPresentStatus()){
+        classesPresent++;       
+    }
+    
+}
+    
+}  
+      if (totalClasses > 0) {
+            double percentage = ((double) classesPresent / totalClasses) * 100;
+            System.out.println("Attendance for " + s.getName() + ": " + percentage + "%");
+        } else {
+            System.out.println("No records found.");
+      }  
+  }  
+
+    public ArrayList<Attendance> getAttendanceList() {
+        return attendanceList;
+    }
+
+    public void getAttendanceReport() {
+        for (Attendance a : attendanceList) {
+            System.out.println("Student: " + a.getStudent().getName() + 
+                               " | Status: " + (a.getPresentStatus() ? "Present" : "Absent"));
+        }
+    
+ 
+}
+
+
+}
+
