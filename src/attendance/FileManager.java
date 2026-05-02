9f4dbc7b7f0f2da729af4796a5be7040cef97261
Soorya <66250855+soorya1723@users.noreply.github.com>
2026-05-03 02:07:32 +0530

    Update FileManager.java

--- src/attendance/FileManager.java
+++ src/attendance/FileManager.java
@@ -2,15 +2,28 @@ package attendance;
 
 import java.util.ArrayList;
 import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
 import java.io.EOFException;
 import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.ObjectInputStream;
-
+import java.io.ObjectOutputStream;
 public class FileManager {
-
     public void saveAttendance(Course c){
-
+        try{
+            FileOutputStream file = new FileOutputStream("attendance.dat");
+            BufferedOutputStream buffop = new BufferedOutputStream(file);
+            ObjectOutputStream objectop = new ObjectOutputStream(buffop);
+            for(Attendance a: c.getAttendanceList()){
+                objectop.writeObject(a);
+            }
+            objectop.close();
+            System.out.println("Attendance updated");
+        }
+        catch(IOException e){
+            System.out.println("Error occured: "+e.getMessage());
+        }
     }
 
     public ArrayList<Attendance> retrieveData(String courseId){
@@ -40,8 +53,23 @@ public class FileManager {
         return readAttendances;
     }
 
-    public void getReport(){
+    public void getReport() {
+        try {
+            FileInputStream file = new FileInputStream("attendance.dat");
+            BufferedInputStream buffinp = new BufferedInputStream(file);
+            ObjectInputStream objectinp = new ObjectInputStream(buffinp);
 
+            while (true) {
+                try {
+                    Attendance a = (Attendance) objectinp.readObject();
+                    System.out.println(a);
+                } catch (EOFException e) {
+                    break; 
+                }
+            }
+            objectinp.close();
+        } catch (IOException | ClassNotFoundException e) {
+            System.out.println("Error while fetching report: " + e.getMessage());
+        }
     }
-
 }
